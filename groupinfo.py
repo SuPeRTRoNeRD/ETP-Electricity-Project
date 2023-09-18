d128b561db9c51f335b04216e2a52274dd0374f6
SuPeRTRoNeRD <supertronerd@pieterpaasman.com>
2023-09-18 11:56:28 +0200

    Add info (Pieter)

--- groupinfo.py
+++ groupinfo.py
@@ -12,9 +12,9 @@ group_member_1_number 	        = 's2666650' 	        # Please enter your student
 group_member_1_study 			= 'ME' 	        # Please enter your study
 
 # Group member 2: 
-group_member_2_name 			= '' 	        # Please enter your name
-group_member_2_number 	        = 's' 	        # Please enter your student number - s1234567 (NOTE, 's' + 7 digits!)
-group_member_2_study 			= '' 	        # Please enter your study
+group_member_2_name 			= 'Pieter Paasman' 	        # Please enter your name
+group_member_2_number 	        = 's2314932' 	        # Please enter your student number - s1234567 (NOTE, 's' + 7 digits!)
+group_member_2_study 			= 'B-EE' 	        # Please enter your study
 
 # Group member 3: 
 group_member_3_name 			= '' 	        # Please enter your name
