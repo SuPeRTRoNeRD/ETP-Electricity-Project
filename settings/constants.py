0bfe635e7e9f28276c14e603067fae0596fd6207
SuPeRTRoNeRD <supertronerd@pieterpaasman.com>
2023-09-18 14:40:30 +0200

    New version of constants.py from canvas

--- settings/constants.py
+++ settings/constants.py
@@ -8,6 +8,10 @@ cfg_sim['timebase'] 		= 900 	# Interval length in seconds
 cfg_sim['intervals'] 		= 7*96 	# Discrete time intervals to simulate
 cfg_sim['startInterval'] 	= 0 	# Discrete time interval to start the simulation
 
+# Definition of Tau
+cfg_sim['ivperhour']            = 3600 / cfg['timebase']  # Intervals per hour
+cfg_sim['tau'] = 1 / (1000*cfg_sim['ivperhour'])  # Conversion factor: Watts for one interval -> kWh for that interval
+
 # Result folder
 cfg_sim['outputfile']       = "output/results.csv"
 cfg_sim['overviewfile']     = "output/overview.csv"
