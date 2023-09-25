dc0ca7258ff9ee1aa7bff4a6fcb2e12f7359c297
SuPeRTRoNeRD <supertronerd@pieterpaasman.com>
2023-09-25 11:24:57 +0200

    Code cleanup assignment 10

--- ev/evprices.py
+++ ev/evprices.py
@@ -9,14 +9,11 @@ def evprices(ev, prices, co2, profile):
     # FIXME: This is a placeholder that needs to be implemented
 
     # result parameter:
-    planning = [0] * len(profile)
+    planning = [0] * len(profile) # initialise as empty list
     soc = ev.evsoc
 
-    available_intervals = {}
+    available_intervals = {} # define as dictionary
 
-    for i in prices:
-        pass
-        
         # What is already given is to determine if the EV is connected to the charging station (at home) or not (driving)
     intervals_per_day = (3600 / cfg_sim['timebase']) * 24
     intervals_per_hour = (3600 / cfg_sim['timebase'])
@@ -37,11 +34,11 @@ def evprices(ev, prices, co2, profile):
 
 
         if i == arrival_interval:
+            # Moment at which the EV arrives
             soc = soc - ev.evenergy
             for j in range(i, departure_interval):
                 available_intervals[j] = prices[j]
 
-            intervals_until_departure = departure_interval - i
             available_intervals = sorted(available_intervals.items(), key=lambda x:x[1])
             soc_needed = ev.evcapacity - soc
             soc_per_interval = ev.evpmax * cfg_sim['tau']
@@ -52,16 +49,6 @@ def evprices(ev, prices, co2, profile):
                 planning[interval[0]] = ev.evpmax
             pass
 
-            # Moment at which the EV arrives
-
-        if i >= arrival_interval and i < departure_interval:
-            # Interval that the EV is connected (available)
-            pass
-
-        else:
-            # Interval that the EV is disconnected (unavailable)
-            pass
-
         if i == departure_interval: #(this statement is unreachable)
             # Moment at which the EV departs
             available_intervals = {}
@@ -70,34 +57,4 @@ def evprices(ev, prices, co2, profile):
 
         # print(planning[interval[0]])
 
-    # print(planning)
-    # NOTE: The following parameters are best used READ-ONLY!
-    # ev parameters can be obtained as follows
-    # ev.evsoc              # State of Charge of the EV in kWh
-    # ev.evminsoc           # Minimum State of Charge of the EV in kWh
-    # ev.evcapacity         # Capacity of the EV in kWh
-    # ev.evpmin             # Minimum power of the EV in W
-    # ev.evpmax             # Maximum power of the EV in W
-
-    # NOTE: The following parameters are best used READ-ONLY!
-    # Timing variables:
-    # ev.evminsoc  	        # Minimum SoC to reach after each session (because of driving)
-    # ev.evconnectiontime   # Hours the EV is connected
-    # ev.evarrivalhour      # Hour of arrival of the EV each day
-
-    # NOTE: The EV needs to be fully charged (i.e., reach maximum SoC) when departing
-
-    # Other input
-    # prices, co2, and profile are vectors (lists) with equal length
-
-    # Fallback implementation: Greedy:
-    # for i in range(0, len(profile)):
-    #     planning.append(ev.evpmax)
-
-    # Finally, the resulting planning for the device must be returned
-    # This is also a list, with each value representing the power consumption (average) during an interval in Watts
-    # The length of this list must be equal to the input vectors (i.e., prices, co2 and profile)
-
-    # For each interval i, these can be set by adding the correction value to the planning list, i.e.:
-    # planning.append(<your_value>)
     return planning
