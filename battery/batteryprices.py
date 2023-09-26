d838c99141297aafa21475a43ad6d36647beec8a
SuPeRTRoNeRD <supertronerd@pieterpaasman.com>
2023-09-26 16:50:46 +0200

    Implement segmenting

--- battery/batteryprices.py
+++ battery/batteryprices.py
@@ -6,10 +6,24 @@ import random
 
 # With this function, a planning for the operation (i.e. control actions) can be implemented
 def batteryprices(battery, prices, co2, profile):
+    planning = []
+    num_of_intervals = len(profile)
+    segment_size = 16
+    num_of_segments = num_of_intervals // segment_size
+    prices_copy = prices.copy()
+    while(len(prices_copy) > segment_size):
+        prices_segment = prices_copy[:segment_size]
+        del prices_copy[:segment_size]
+        planning += plan_segment(battery, prices_segment)
+    planning += plan_segment(battery, prices_copy)
     
-    # result parameter:
+    return planning
+
+def plan_segment(battery, prices):
+    segment = [0] * len(prices)
+
     price_dict = {}
-    planning = [0] * len(profile)   # Initialise empty planning
+    segment = [0] * len(prices)   # Initialise empty segment
     for i in range(0, len(prices)):
         price_dict[i] = prices[i]
     
@@ -19,32 +33,20 @@ def batteryprices(battery, prices, co2, profile):
     for i in price_dict:
         intervals_by_price.append(i[0])
 
-    # Battery parameters can be obtained as follows
-    # battery.batsoc        # State of Charge in kWh
-    # battery.batminsoc     # Minimum State of Charge
-    # battery.batcapacity   # Capacity of the battery in kWh
-    # battery.batpmin       # Minimum power in W (Negative value)
-    # battery.batpmax       # Maximum power in W
-
-    # Other input
-    # prices, co2, and profile are vectors (lists) with equal length
-
-    # intervals_by_price = '1'
     while(len(intervals_by_price) > 1):
-        # calculate soc for each interval
+    # calculate soc for each interval
         soc = [battery.batsoc]
-        for i in range(0, len(planning)):
-            soc.append(soc[i] + (planning[i] * cfg_sim['tau']))
+        for i in range(0, len(segment)):
+            soc.append(soc[i] + (segment[i] * cfg_sim['tau']))
         
         # lowest price is during interval intervals_by_price[0]
         # highest price is during interal intevals_by_price[-1]
         cheap_interval = intervals_by_price.pop(0)
-        planning[cheap_interval] = confine_p(battery.batpmax, battery, soc[cheap_interval])
+        segment[cheap_interval] = confine_p(battery.batpmax, battery, soc[cheap_interval])
         expensive_interval = intervals_by_price.pop(-1)
-        planning[expensive_interval] = confine_p(battery.batpmin, battery, soc[expensive_interval])
+        segment[expensive_interval] = confine_p(battery.batpmin, battery, soc[expensive_interval])
 
-
-    return planning
+    return segment
 
 def confine_p(p_desired, battery, soc): # Make sure p is within battery limits (both p and soc)
     p = p_desired
