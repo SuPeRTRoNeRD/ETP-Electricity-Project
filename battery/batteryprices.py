7cbc2608cbed52cc37efd669f75ee97203799034
SuPeRTRoNeRD <supertronerd@pieterpaasman.com>
2023-09-26 12:19:00 +0200

    Implement assignment 12

--- battery/batteryprices.py
+++ battery/batteryprices.py
@@ -6,10 +6,18 @@ import random
 
 # With this function, a planning for the operation (i.e. control actions) can be implemented
 def batteryprices(battery, prices, co2, profile):
-    # FIXME: This is a placeholder that needs to be implemented
-
+    
     # result parameter:
-    planning = []
+    price_dict = {}
+    planning = [0] * len(profile)   # Initialise empty planning
+    for i in range(0, len(prices)):
+        price_dict[i] = prices[i]
+    
+    price_dict = sorted(price_dict.items(), key=lambda x:x[1])
+
+    intervals_by_price = []         # List of all intervals, sorted by price from lowest to highest
+    for i in price_dict:
+        intervals_by_price.append(i[0])
 
     # Battery parameters can be obtained as follows
     # battery.batsoc        # State of Charge in kWh
@@ -21,15 +29,36 @@ def batteryprices(battery, prices, co2, profile):
     # Other input
     # prices, co2, and profile are vectors (lists) with equal length
 
-    # FIXME: Placeholder implementation
-    # Fallback implementation: Greedy:
-    for i in range(0, len(profile)):
-        planning.append(-profile[i])
+    # intervals_by_price = '1'
+    while(len(intervals_by_price) > 1):
+        # calculate soc for each interval
+        soc = [battery.batsoc]
+        for i in range(0, len(planning)):
+            soc.append(soc[i] + (planning[i] * cfg_sim['tau']))
+        
+        # lowest price is during interval intervals_by_price[0]
+        # highest price is during interal intevals_by_price[-1]
+        cheap_interval = intervals_by_price.pop(0)
+        planning[cheap_interval] = confine_p(battery.batpmax, battery, soc[cheap_interval])
+        expensive_interval = intervals_by_price.pop(-1)
+        planning[expensive_interval] = confine_p(battery.batpmin, battery, soc[expensive_interval])
 
-    # Finally, the resulting planning for the device must be returned
-    # This is also a list, with each value representing the power consumption (average) during an interval in Watts
-    # The length of this list must be equal to the input vectprs (i.e., prices, co2 and profile)
 
-    # For each interval i, these can be set by adding the correction value to the planning list, i.e.:
-    # planning.append(<your_value>)
     return planning
+
+def confine_p(p_desired, battery, soc): # Make sure p is within battery limits (both p and soc)
+    p = p_desired
+    if p < battery.batpmin:
+        p = battery.batpmin
+    elif p > battery.batpmax:
+        p = battery.batpmax
+    
+    soc_next = soc + p * cfg_sim['tau']
+    if soc_next < battery.batminsoc:
+        p = (battery.batminsoc - soc)/cfg_sim['tau']
+        soc_next = battery.batminsoc
+    elif soc_next > battery.batcapacity:
+        p = (battery.batcapacity - soc)/cfg_sim['tau']
+        soc_next = battery.batcapacity
+
+    return p
